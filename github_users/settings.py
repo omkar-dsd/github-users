f5d2c56278f409cbe727bdd4f8bc076ebe854b1e
Omkar <omkar@dataweave.com>
2018-06-23 17:26:06 +0530

    Required intalled apps

--- github_users/settings.py
+++ github_users/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'users_api',
+    'rest_framework',
+    'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
